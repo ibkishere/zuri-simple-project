037f9e2c3f130a1b0d029e163e0ddefda3f3f362
ibukunoluwa Akintobi <88123302+ibkishere@users.noreply.github.com>
2022-06-24 23:14:06 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const info ={
+    name: 'Ibukunoluwa Akintoib',
+    height: '6feet',
+    country: 'Nigeria',
+}
+
+alert(
+    `My name is ${info.name} , i am ${info.height} tall and i  am from ${info.country}.`
+);
